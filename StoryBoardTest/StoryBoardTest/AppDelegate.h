2ae045bf6d51a7240c60ee60f059eaa433e22280
apple <>
2018-01-11 17:34:11 +0800

    添加storyboard测试

---
+++ StoryBoardTest/StoryBoardTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StoryBoardTest
+//
+//  Created by apple on 2018/1/11.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StoryBoardTest/StoryBoardTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StoryBoardTest
+//
+//  Created by apple on 2018/1/11.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
